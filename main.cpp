ca6fdd0047e0c8961fc9b0ae22ff664c8cffa827
geo_rioz <91685749+Geo52@users.noreply.github.com>
2025-05-04 21:01:22 +0000

    damians code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,35 @@
+#include "Autocompleter.h"
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
-int main() { std::cout << "Hello World!\n"; }
+int main() {
+  Autocompleter ac;
+   // Insert some words into the Autocompleter
+  vector<string> words = {"aardvark", "albatross", "alpaca",    "armadillo",
+                          "cat",      "camel",     "crocodile", "crow",
+                          "giraffe",  "goat",      "goose",     "goatfish",
+                          "gorilla"};
+  for (const string &word : words) {  // Insert each word into the Autocompleter
+    ac.insert(word);
+  }
+  cout << "Tree size: " << ac.size() << endl; 
+  cout << "Tree height: " << ac.height() << endl; 
+
+  cout << "Completions for prefix 'a':" << endl;  
+  ac.completions("a"); // Output: aardvark alpaca armadillo
+
+  string userPrefix;
+  while (true) {
+    cout << "Enter a prefix to search for: (use 'exit' to stop) " << endl; 
+    getline(cin, userPrefix);
+    if (userPrefix == "exit") {
+      break;
+    }
+
+    cout << "Completions for prefix '" << userPrefix << "':" << endl; // Output:
+    ac.completions(userPrefix); 
+  }
+  return 0;
+}
